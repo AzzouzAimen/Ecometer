ad4c3e8434ab5e4718fedfde1b97d916b14fffbd
idrissziadi <idriss.ziadi47@gmail.com>
2024-04-22 21:29:58 +0200

    Modif 2

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css';
+import Navbar from './Navbar.jsx';
+import Hero from './Hero.jsx';
+import Sectionii from './Sectionii.jsx';
+import Calculateur from './Calculateur.jsx';
+function App() {
+  
+
+  return (
+    <section>
+      <Calculateur />
+      </section>
+    
+    
+    
+  );
+}
+
+export default App;
