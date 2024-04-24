a5b98c2dd37013dab817ca6d7a4c61dd04502b03
idrissziadi <idriss.ziadi47@gmail.com>
2024-04-24 23:56:48 +0200

    nouvelleBranche

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css';
+import Navbar from './Navbar.jsx';
+import Hero from './Hero.jsx';
+import Sectionii from './Sectionii.jsx';
+import Calculateur from './Auth/Pages/Calculateur.jsx';
+import EmissionsDirectes from './Auth/Components/EmissionsDirectes.jsx';
+import AppBarComponent from './Auth/Components/AppBarComponent.jsx';
+import Acceuil from './Auth/Pages/Accueil.jsx';
+function App() {
+  
+
+  return (
+    <section>
+      <Acceuil />
+    </section> 
+  );
+}
+
+export default App;
---
+++ frontend/src/Auth/Components/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideBar = () => {
+    return (
+        <div style={{ backgroundColor: '#003049' , height : '100%'}}>
+            
+        </div>
+    );
+}
+
+export default SideBar;
